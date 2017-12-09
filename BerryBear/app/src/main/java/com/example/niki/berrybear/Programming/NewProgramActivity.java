69215b9da043b8821bd65e9823ef123d40e04c21
NikoletaDalaktchieva <nikoletadal@gmail.com>
2017-12-09 18:26:13 +0200

    Add other elements in NewProgramActivity

--- BerryBear/app/src/main/java/com/example/niki/berrybear/Programming/NewProgramActivity.java
+++ BerryBear/app/src/main/java/com/example/niki/berrybear/Programming/NewProgramActivity.java
@@ -1,11 +1,11 @@
 package com.example.niki.berrybear.Programming;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.DragEvent;
@@ -16,11 +16,14 @@ import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnLongClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.niki.berrybear.Adapters.SimpleImageArrayAdapter;
 import com.example.niki.berrybear.MainActivity;
 import com.example.niki.berrybear.R;
 
@@ -30,22 +33,18 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewProgramActivity extends ActionBarActivity {
-
-    String[] comands ={
-            "Up",
-            "Down",
-            "Left",
-            "Right"
-    };
+public class NewProgramActivity extends Activity {
 
     public static List<Integer> imageId  = new ArrayList<Integer>();
 
     LinearLayout area1, area2;
-    View view = null;
+    //View view = null;
     boolean drag = false;
     Bundle tempBundle;
     public static boolean openTab2 = false;
+    NewProgramActivity forNewViews;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +58,13 @@ public class NewProgramActivity extends ActionBarActivity {
         imageId.add(R.mipmap.ic_down);
         imageId.add(R.mipmap.ic_left);
         imageId.add(R.mipmap.ic_up);
+        imageId.add(R.mipmap.ic_light_on);
+        imageId.add(R.mipmap.ic_light_off);
 
         area1 = (LinearLayout) findViewById(R.id.area1);
         area2 = (LinearLayout) findViewById(R.id.area2);
 
-
+        forNewViews = this;
         TypedArray arrayResources = getResources().obtainTypedArray(
                 R.array.resicon);
 
@@ -73,10 +74,6 @@ public class NewProgramActivity extends ActionBarActivity {
 
         area1.setOnDragListener(myOnDragListener);
         area2.setOnDragListener(myOnDragListener);
-
-        if(drag){
-            Log.e("Drag", "Drag'");
-        }
 
     }
 
@@ -102,13 +99,42 @@ public class NewProgramActivity extends ActionBarActivity {
                 case DragEvent.ACTION_DRAG_ENTERED: break;
                 case DragEvent.ACTION_DRAG_EXITED: break;
                 case DragEvent.ACTION_DROP:
-                    if(v == area1) break;
-                    View view = (View)event.getLocalState();
-                    LinearLayout oldParent = (LinearLayout)view.getParent();
-                    oldParent.removeAllViews();
-                    LinearLayout newParent = (LinearLayout)v;
-                    newParent.addView(view);
-                    addImg();
+                    if(v == area1){
+                        View view = (View) event.getLocalState();
+                        LinearLayout oldParent = (LinearLayout) view.getParent();
+                        oldParent.removeView(view);
+                    }else {
+                        View view = (View) event.getLocalState();
+                        LinearLayout oldParent = (LinearLayout) view.getParent();
+                        oldParent.removeAllViews();
+                        LinearLayout newParent = (LinearLayout) v;
+
+
+                        if((view instanceof Button) && ((Button) view).getText() == "Forward"){
+                            LinearLayout layout = new LinearLayout(forNewViews);
+                            Spinner spinner = new Spinner(forNewViews);
+                            SimpleImageArrayAdapter adapter = new SimpleImageArrayAdapter(forNewViews,
+                                    new Integer[]{R.mipmap.ic_up, R.mipmap.ic_down, R.mipmap.ic_left ,R.mipmap.ic_right});
+                            spinner.setAdapter(adapter);
+                            spinner.setLayoutParams(new Spinner.LayoutParams(Spinner.LayoutParams.WRAP_CONTENT, Spinner.LayoutParams.WRAP_CONTENT));
+                            layout.addView(view);
+                            layout.addView(spinner);
+                            layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            newParent.addView(layout);
+                        }else if((view instanceof Button) && ((Button) view).getText() == "Wait"){
+                            LinearLayout layout = new LinearLayout(forNewViews);
+                            EditText text = new EditText(forNewViews);
+                            text.setHint("1s");
+                            layout.addView(view);
+                            layout.addView(text);
+                            layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            newParent.addView(layout);
+
+                        }else newParent.addView(view);
+
+                        addImg();
+                    }
+
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
                 default:
@@ -130,6 +156,21 @@ public class NewProgramActivity extends ActionBarActivity {
             imageView.setOnLongClickListener(myOnLongClickListener);
             area1.addView(imageView);
         }
+
+        Button button1 = new Button(this);
+        button1.setText("Forward");
+        button1.setOnLongClickListener(myOnLongClickListener);
+        area1.addView(button1);
+
+        Button button2 = new Button(this);
+        button2.setText("Stop");
+        button2.setOnLongClickListener(myOnLongClickListener);
+        area1.addView(button2);
+
+        Button button3 = new Button(this);
+        button3.setText("Wait");
+        button3.setOnLongClickListener(myOnLongClickListener);
+        area1.addView(button3);
     }
 
     void sendJSON(String ProgramName, String commands){
@@ -183,6 +224,8 @@ public class NewProgramActivity extends ActionBarActivity {
                 }else if (getResources().getDrawable(R.mipmap.ic_right).getConstantState() == ((ImageView) view).getDrawable().getConstantState()) {
                     newCommands.add("right");
                 }
+            }else if(view instanceof Button){
+
             }else Log.e("View", "Not Image");
 
         }
