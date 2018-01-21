bf637561b42f6d6671223db1450d5cc25a339a8a
NikoletaDalaktchieva <nikoletadal@gmail.com>
2018-01-21 14:41:43 +0200

    Change IP adress

--- BerryBear/app/src/main/java/com/example/niki/berrybear/HttpRequests/URLS.java
+++ BerryBear/app/src/main/java/com/example/niki/berrybear/HttpRequests/URLS.java
@@ -2,5 +2,27 @@ package com.example.niki.berrybear.HttpRequests;
 
 
 public class URLS {
+    public static String ip;
+
+    public static String getProgramURl(){
+        return "http://" + ip + "/programs/";
+    }
+
+    public static String getRobotURl(){
+        return "http://" + ip + "/robots/1";
+    }
+
+    public static String getIdURl(int id){
+        return URLS.getProgramURl() + String.valueOf(id) + "/";
+    }
+
+    public static void setIP(String ip){
+        URLS.ip = ip;
+    }
+
+    public static String getIP(){
+        return URLS.ip;
+    }
+
 
 }
--- BerryBear/app/src/main/java/com/example/niki/berrybear/Tabs/InformationActivity.java
+++ BerryBear/app/src/main/java/com/example/niki/berrybear/Tabs/InformationActivity.java
@@ -1,15 +1,76 @@
 package com.example.niki.berrybear.Tabs;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.niki.berrybear.HttpRequests.URLS;
 import com.example.niki.berrybear.R;
 
+import java.util.Objects;
+
 public class InformationActivity extends Activity {
+    private SharedPreferences prefs;
+
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editPrefs = prefs.edit();
+        editPrefs.putString("ip", URLS.getIP());
+        editPrefs.commit();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        URLS.setIP(prefs.getString("ip", "192..."));
+
+        final LinearLayout ipLayout = (LinearLayout) findViewById(R.id.ipLayout);
+        final TextView IP = new TextView(this);
+        IP.setTextSize(20);
+        final EditText editIP = new EditText(this);
+        editIP.setHint("IP...");
+        editIP.setTextSize(20);
+
+
+        IP.setText(URLS.getIP());
+        ipLayout.addView(IP);
+
+        final Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                Log.e("Button", String.valueOf(button.getText()));
+                if(Objects.equals(String.valueOf(button.getText()), "Edit")) {
+                    editIP.setText(IP.getText());
+                    ipLayout.removeView(IP);
+                    ipLayout.addView(editIP);
+                    button.setText("Save");
+                    button.setBackgroundResource(R.drawable.green_round_button);
+                }else {
+
+                    IP.setText(editIP.getText());
+                    ipLayout.removeView(editIP);
+                    ipLayout.addView(IP);
+                    button.setText("Edit");
+                    button.setBackgroundResource(R.drawable.red_round_button);
+                    URLS.setIP(String.valueOf(editIP.getText()));
+                }
+            }
+
+        });
     }
 }
